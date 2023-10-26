f0fa4cafba6a00426655a1daa4ab9de696b70a46
Samuel Martins <samuelsilvaa431@gmail.com>
2023-10-26 14:44:29 -0300

    Revisão sobre generics

---
+++ Generics/src/App.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        List <String> lista = new ArrayList<>();
+        lista.add("oi");
+        lista.add(12); //Não permitido pois essa lista recebe apenas Strings
+
+        
+    }
+}
